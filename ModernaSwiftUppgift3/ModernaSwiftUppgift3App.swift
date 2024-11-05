e1b451ffbc87e3f617747b32eb2a2537e7677e02
Andreas Nef <andreas@ontherecord.se>
2024-11-05 16:57:02 +0100

    Initial Commit

---
+++ ModernaSwiftUppgift3/ModernaSwiftUppgift3App.swift
@@ -0,0 +1,17 @@
+//
+//  ModernaSwiftUppgift3App.swift
+//  ModernaSwiftUppgift3
+//
+//  Created by Andreas Nef on 05.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ModernaSwiftUppgift3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ModernaSwiftUppgift3Tests/ModernaSwiftUppgift3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ModernaSwiftUppgift3Tests.swift
+//  ModernaSwiftUppgift3Tests
+//
+//  Created by Andreas Nef on 05.11.2024.
+//
+
+import Testing
+@testable import ModernaSwiftUppgift3
+
+struct ModernaSwiftUppgift3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
